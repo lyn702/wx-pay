3800d10558ba1aa16d3bf53e3d7d5d6e6501df7f
towsu <1029187668@qq.com>
2017-12-01 16:17:18 +0800

    Liyn
    1号，微信支付 输入总金额定位规则改变

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let log = console.log.bind(console)
+
 // 获得时间
 let time = function(z) {
     if (z === undefined) {
@@ -20,6 +22,75 @@ let time = function(z) {
     return `${Year}-${Month}-${Day}`
 }
 time()
+
+let money = document.querySelector('#input-money')
+// log(money)
+let qian = money.value
+
+// 点击按钮事件
+let click = function () {
+    $('#one').on('click', function() {
+        // log('1')
+        // let qian = money.value
+        log(qian)
+        qian = qian + 1
+        log(qian)
+    })
+
+    $('#two').on('click', function() {
+        log('2')
+    })
+
+    $('#three').on('click', function() {
+        log('3')
+    })
+
+    $('#back').on('click', function() {
+        log('返回')
+    })
+
+    $('#four').on('click', function() {
+        log('4')
+    })
+
+    $('#five').on('click', function() {
+        log('5')
+    })
+
+    $('#six').on('click', function() {
+        log('6')
+    })
+
+    $('#seven').on('click', function() {
+        log('7')
+    })
+
+    $('#eight').on('click', function() {
+        log('8')
+    })
+
+    $('#nine').on('click', function() {
+        log('9')
+    })
+
+    $('#zero').on('click', function() {
+        log('0')
+    })
+
+    $('#dian').on('click', function() {
+        log('.')
+    })
+
+    $('#Ok').on('click',function() {
+        log('Ok')
+        // let qian = money.value
+        // log(qian)
+    })
+}
+click()
+
+
+
 
 let onBridgeReady = function() {
     WeixinJSBridge.invoke(
